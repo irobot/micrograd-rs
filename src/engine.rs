7224e7de0f0614198415635f971734c1efa93b56
pax <gh@noctis.pro>
2024-01-22 22:50:34 +0000

    Replace dyn Op traits with Expr enum

--- src/engine.rs
+++ src/engine.rs
@@ -1,146 +1,113 @@
 use std::{
     cell::RefCell,
     collections::HashSet,
-    fmt, ops,
+    fmt,
+    ops,
     rc::Rc,
     sync::atomic::{AtomicUsize, Ordering},
 };
 
 type Data = f64;
 
-pub struct Val {
+pub struct Value {
     pub id: usize,
-    pub name: String,
     pub data: Data,
     pub grad: RefCell<Data>,
-    pub op: Box<dyn Op>,
+    pub op: Expr,
+}
+
+#[derive(Clone)]
+pub struct Node {
+    pub node: Rc<Value>,
+    pub name: String,
+}
+
+pub enum Expr {
+    Leaf,
+    Const(Node),
+    Add(Node, Node),
+    Mul(Node, Node),
+    Relu(Node),
 }
 
 pub trait Op: Backprop {
-    fn get_inputs(&self) -> Vec<&Val>;
+    fn get_inputs(&self) -> Vec<&Node>;
 }
 
 pub trait Backprop: std::fmt::Display {
     fn backward(&self, grad: Data);
 }
 
-struct Add {
-    left: Rc<Val>,
-    right: Rc<Val>,
-}
-
-impl Backprop for Add {
+impl Backprop for Expr {
     fn backward(&self, grad: Data) {
-        self.left.add_grad(grad);
-        self.right.add_grad(grad);
+        match self {
+            Expr::Add(left, right) => {
+                left.add_grad(grad);
+                right.add_grad(grad);
+            }
+            Expr::Mul(left, right) => {
+                left.add_grad(grad * right.data());
+                right.add_grad(grad * left.data());
+            }
+            Expr::Relu(input) => {
+                let d =  if input.data() >= 0. { 1.} else { 0. };
+                input.add_grad(grad * d)
+            },
+            Expr::Const(_) => (),
+            Expr::Leaf => (),
+        }
     }
 }
 
-impl Op for Add {
-    fn get_inputs(&self) -> Vec<&Val> {
-        vec![self.left.as_ref(), self.right.as_ref()]
+impl Op for Expr {
+    fn get_inputs(&self) -> Vec<&Node> {
+        match self {
+            Expr::Add(left, right) => vec![left, right],
+            Expr::Mul(left, right) => vec![left, right],
+            Expr::Relu(input) => vec![input],
+            Expr::Const(_) => vec![],
+            Expr::Leaf => vec![],
+        }
     }
 }
 
-impl fmt::Display for Add {
+impl fmt::Display for Expr {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let ld = self.left.data;
-        let rd = self.right.data;
-        write!(f, "= {} + {}", ld, rd)
+        match self {
+            Expr::Relu(input) => write!(f, "= ReLU({})", input.data()),
+            Expr::Add(left, right) => write!(f, "= {} + {}", left.data(), right.data()),
+            Expr::Mul(left, right) => write!(f, "= {} * {}", left.data(), right.data()),
+            Expr::Const(v) => write!(f, "{}", v.node.data),
+            Expr::Leaf => Result::Ok(()),
+        }
     }
-}
-
-struct Mul {
-    left: Rc<Val>,
-    right: Rc<Val>,
-}
-
-impl Backprop for Mul {
-    fn backward(&self, grad: Data) {
-        self.left.add_grad(grad * self.right.data);
-        self.right.add_grad(grad * self.left.data);
-    }
-}
-
-impl Op for Mul {
-    fn get_inputs(&self) -> Vec<&Val> {
-        vec![self.left.as_ref(), self.right.as_ref()]
-    }
-}
-
-impl fmt::Display for Mul {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let ld = self.left.data;
-        let rd = self.right.data;
-        write!(f, "= {} * {}", ld, rd)
-    }
-}
-
-struct Relu {
-    value: Rc<Val>
 }
 
 fn relu(v: Data) -> Data {
-    if v > 0. { v } else { 0. }
-}
-
-impl Backprop for Relu {
-    fn backward(&self, grad: Data) {
-        self.value.add_grad(grad * relu(self.value.data));
+    if v > 0. {
+        v
+    } else {
+        0.
     }
 }
 
-impl Op for Relu {
-    fn get_inputs(&self) -> Vec<&Val> {
-        vec![self.value.as_ref()]
-    }
-}
-
-impl fmt::Display for Relu {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "= ReLU({})", self.value.data)
-    }
-}
-
-struct Noop {}
-
-impl Backprop for Noop {
-    fn backward(&self, _: Data) {
-        ()
-    }
-}
-
-impl Op for Noop {
-    fn get_inputs(&self) -> Vec<&Val> {
-        vec![]
-    }
-}
-
-const NOOP: Noop = Noop {};
-
-impl fmt::Display for Noop {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "")
-    }
-}
-
-pub fn topological_sort(v: &Val) -> Vec<&Val> {
-    let mut topo: Vec<&Val> = vec![];
+pub fn topological_sort(v: &Node) -> Vec<&Node> {
+    let mut topo: Vec<&Node> = vec![];
     let mut visited = HashSet::<usize>::new();
-    let mut stack: Vec<&Val> = vec![];
+    let mut stack: Vec<&Node> = vec![];
 
     stack.push(v);
-    visited.insert(v.id);
+    visited.insert(v.node.id);
 
     while stack.len() > 0 {
         // last is guaranteed to be Some
         // due to the condition in the line above
         let head = *stack.last().unwrap();
-        let inputs = head.op.get_inputs(); 
-        let next = inputs.iter().find(|c| !visited.contains(&c.id));
+        let inputs = head.node.op.get_inputs();
+        let next = inputs.iter().find(|c| !visited.contains(&c.node.id));
         if let Some(h) = next {
             stack.push(h);
-            visited.insert(h.id);
+            visited.insert(h.node.id);
             continue;
         }
         topo.push(&head);
@@ -151,127 +118,222 @@ pub fn topological_sort(v: &Val) -> Vec<&Val> {
 
 static OBJECT_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
-impl Val {
-    pub fn make(
-        v: Data,
-        name: Option<String>,
-        op: Box<dyn Op>,
-    ) -> Val {
+impl Value {
+    pub fn make(data: Data, op: Expr) -> Value {
         let id = OBJECT_COUNTER.fetch_add(1, Ordering::SeqCst);
-        Val {
+        Value {
             id,
-            name: name.unwrap_or(id.to_string()),
-            data: v,
+            data,
             grad: RefCell::new(0.),
             op,
         }
     }
 
-    pub fn new(v: Data) -> Val {
-        Val::make(v, None, Box::new(NOOP))
+    pub fn new(v: Data) -> Value {
+        Value::make(v, Expr::Leaf)
     }
 
-    pub fn named(v: Data, name: &str) -> Val {
-        Val::make(v, Some(String::from(name)), Box::new(NOOP))
-    }
-
-    pub fn from_op(v: Data, op: Box<dyn Op>) -> Val {
-        Val::make(v, None, op)
+    pub fn from_op(v: Data, op: Expr) -> Value {
+        Value::make(v, op)
     }
 
     pub fn add_grad(&self, delta_grad: Data) {
         *(self.grad.borrow_mut()) += delta_grad;
     }
 
-    pub fn relu(self) -> Val {
-        let data = self.data;
-        Val::from_op(relu(data), Box::new(Relu { value: Rc::new(self)}))
+    pub fn set_grad(&self, grad: Data) {
+        *self.grad.borrow_mut() = grad;
     }
 
     pub fn backward(&self) {
-        let topo = topological_sort(self);
+        self.op.backward(*(self.grad.borrow()));
+    }
+}
 
-        for v in topo.iter().rev() {
-            v.op.backward(*(v.grad.borrow()));
+impl fmt::Display for Node {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let grad = self.node.grad.borrow();
+        write!(
+            f,
+            "{} => Value({} {}, grad={})",
+            self.name, self.node.data, self.node.op, grad
+        )
+    }
+}
+
+impl Default for Node {
+    fn default() -> Self {
+        Self {
+            node: Rc::new(Value::new(0.)),
+            name: Default::default(),
         }
     }
 }
 
-impl fmt::Display for Val {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let grad = self.grad.borrow();
-        write!(
-            f,
-            "{} => Value({} {}, grad={})",
-            self.name, self.data, self.op, grad
-        )
+impl Node {
+    pub fn new(v: Data) -> Node {
+        Node::from(Value::new(v))
+    }
+
+    pub fn from(v: Value) -> Node {
+        let name = v.id.to_string();
+        Node {
+            node: Rc::new(v),
+            name,
+        }
+    }
+
+    pub fn named(v: Data, name: &str) -> Node {
+        Node {
+            node: Rc::new(Value::new(v)),
+            name: name.to_string(),
+        }
+    }
+
+    pub fn name(&mut self, name: &str) -> Node {
+        Node {
+            node: self.node.clone(),
+            name: name.to_string(),
+        }
+    }
+
+    pub fn data(&self) -> Data {
+        self.node.data
+    }
+
+    pub fn relu(&self) -> Node {
+        let data = self.data();
+        let name = self.node.id.to_string();
+        Node {
+            node: Rc::new(Value::from_op(relu(data), Expr::Relu(self.clone()))),
+            name,
+        }
+    }
+
+    fn add_grad(&self, delta_grad: Data) {
+        self.node.add_grad(delta_grad)
+    }
+
+    pub fn backward(&self) {
+        let topo = topological_sort(self);
+        for v in topo.iter() {
+            v.node.set_grad(0.);
+        }
+        self.node.set_grad(1.);
+        for v in topo.iter().rev() {
+            v.node.backward();
+        }
     }
 }
 
-impl ops::Mul for Val {
-    type Output = Val;
-
-    fn mul(self, _rhs: Val) -> Val {
-        let left = Rc::new(self);
-        let right = Rc::new(_rhs);
-        Val::from_op(
-            left.data * right.data,
-            Box::new(Mul { left, right }),
-        )
+impl ops::Mul for Node {
+    type Output = Node;
+    fn mul(self, _rhs: Node) -> Node {
+        &self * &_rhs
     }
 }
 
-impl ops::Mul<f64> for Val {
-    type Output = Val;
-
-    fn mul(self, _rhs: f64) -> Val {
-        self * Val::new(_rhs)
+impl ops::Mul<Node> for &Node {
+    type Output = Node;
+    fn mul(self, _rhs: Node) -> Node {
+        self * &_rhs
     }
 }
 
-impl ops::Add for Val {
-    type Output = Val;
-
-    fn add(self, _rhs: Val) -> Val {
-        let left = Rc::new(self);
-        let right = Rc::new(_rhs);
-        Val::from_op(
-            left.data + right.data,
-            Box::new(Add { left, right }),
-        )
+impl ops::Mul<&Node> for Node {
+    type Output = Node;
+    fn mul(self, _rhs: &Node) -> Node {
+        &self * _rhs
     }
 }
 
-impl ops::Add<f64> for Val {
-    type Output = Val;
+impl ops::Mul for &Node {
+    type Output = Node;
+    fn mul(self, _rhs: &Node) -> Node {
+        Node::from(Value::from_op(
+            self.data() * _rhs.data(),
+            Expr::Mul(self.clone(), _rhs.clone()),
+        ))
+    }
+}
 
-    fn add(self, _rhs: f64) -> Val {
-        self + Val::new(_rhs)
+impl ops::Mul<f64> for &Node {
+    type Output = Node;
+    fn mul(self, _rhs: f64) -> Node {
+        self * &Node::from(Value::new(_rhs))
+    }
+}
+
+impl ops::Mul<f64> for Node {
+    type Output = Node;
+    fn mul(self, _rhs: f64) -> Node {
+        &self * _rhs
+    }
+}
+
+
+impl ops::Add<Node> for Node {
+    type Output = Node;
+    fn add(self, _rhs: Node) -> Node {
+        &self + &_rhs
+    }
+}
+
+impl ops::Add<Node> for &Node {
+    type Output = Node;
+    fn add(self, _rhs: Node) -> Node {
+        self + &_rhs
+    }
+}
+
+impl ops::Add<&Node> for Node {
+    type Output = Node;
+    fn add(self, _rhs: &Node) -> Node {
+        &self + _rhs
+    }
+}
+
+impl ops::Add for &Node {
+    type Output = Node;
+    fn add(self, _rhs: &Node) -> Node {
+        let left = self;
+        let right = _rhs;
+        Node::from(Value::from_op(
+            left.data() + right.data(),
+            Expr::Add(left.clone(), right.clone()),
+        ))
+    }
+}
+
+impl ops::Add<f64> for &Node {
+    type Output = Node;
+    fn add(self, _rhs: f64) -> Node {
+        self + &Node::from(Value::new(_rhs))
+    }
+}
+
+impl ops::Add<f64> for Node {
+    type Output = Node;
+    fn add(self, _rhs: f64) -> Node {
+        &self + _rhs
     }
 }
 
 #[cfg(test)]
 mod test {
 
-    use std::rc::Rc;
-
-    use crate::engine::{topological_sort, Val};
+    use crate::engine::{topological_sort, Node};
 
     #[test]
     fn test_topo_sort() {
-        println!("Hi!");
+        let a = Node::named(1., "a");
+        let b = Node::named(2., "b");
+        let c = (a + b).name("c");
 
-        let a = Val::named(1., "a");
-        let b = Val::named(2., "b");
-        let mut c = a + b;
-        c.name = String::from("c");
-
-        let d = Val::named(3., "d");
-        let e = Val::named(4., "e");
-        let mut f = d * e;
-        f.name = String::from("f");
-        let mut g = f + c;
-        g.name = String::from("g");
+        let d = Node::named(3., "d");
+        let e = Node::named(4., "e");
+        let f = (d * e).name("f");
+        let g = (f + c).name("g");
 
         let topo = topological_sort(&g);
         let mut order = String::from("");
@@ -283,38 +345,36 @@ mod test {
 
     #[test]
     fn test_scalar_add() {
-        assert_eq!((Val::new(1.) + 2.).data, 3.);
+        assert_eq!((Node::new(1.) + 2.).data(), 3.);
     }
 
     #[test]
     fn test_scalar_mul() {
-        assert_eq!((Val::new(7.) * 6.).data, 42.);
+        assert_eq!((Node::new(7.) * 6.).data(), 42.);
     }
 
     #[test]
     fn test_relu_pos() {
-        assert_eq!(Val::new(10.).relu().data, 10.);
+        assert_eq!(Node::new(10.).relu().data(), 10.);
     }
 
     #[test]
     fn test_relu_neg() {
-        assert_eq!(Val::new(-10.).relu().data, 0.);
+        assert_eq!(Node::new(-10.).relu().data(), 0.);
     }
 
     #[test]
     fn test_sanity_check() {
+        let x = Node::named(-4.0, "x");
+        let z = ((&x * 2.0).name("z0") + (&x + 2.).name("z1")).name("z");
+        let q = (&z.relu() + (&z * &x).name("q1")).name("q");
+        let h = ((&z * &z).relu()).name("h");
+        let y = ((h + &q).name("y0") + (q * &x).name("y1")).name("y");
+        y.backward();
 
-        // let xrc = Rc::new(Val::new(-4.0));
-        // let x = xrc.clone();
-        // let z = *x * 2.0 + *x + 2.;
-        // let q = z.relu() + z * *x;
-        // let h = (z * z).relu();
-        // let y = h + q + q * *x;
-        // y.backward();
-
-        // // forward pass went well
-        // assert_eq!(y.data, 5.);
-        // // backward pass went well
-        // assert_eq!(*(x.grad.borrow()), 5.);
+        // forward pass went well
+        assert_eq!(y.data(), -20.);
+        // backward pass went well
+        assert_eq!(*(x.node.grad.borrow()), 46.);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
-use crate::engine::{Val, topological_sort};
+use engine::Node;
+
+use crate::engine::topological_sort;
 
 mod engine;
 
 fn main() {
-    let a = Val::named(1., "a");
-    let b = Val::named(2., "b");
-    let mut c = a + b;
-    c.name = String::from("c");
+    let a = Node::named(1., "a");
+    let b = Node::named(2., "b");
+    let c = (a + b).name("c");
 
-    let e = Val::named(4., "e");
-    let f = Val::named(5., "f");
-    let mut g = e * f;
-    g.name = String::from("g");
+    let e = Node::named(4., "e");
+    let f = Node::named(5., "f");
+    let g = (e * f).name("g");
 
-    let mut h = c * g;
-    h.name = String::from("h");
-    *h.grad.borrow_mut() = 1.0;
-    h.backward();
+    let h = (c * g).name("h");
 
-    let topo = topological_sort(&h);
+    let i = h.relu();
+    i.backward();
+
+    let topo = topological_sort(&i);
     for v in topo.iter() {
         println!("{}", v);
     }
